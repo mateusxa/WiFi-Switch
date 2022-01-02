98123cacc369fc7a31107b7887c20218ac8f3262
Mateus Xavier <mateus_xa@hotmail.com>
2022-01-02 19:38:57 -0300

    Timer 4 / Blue PWM on aprox. 2 kHz

--- main.c
+++ main.c
@@ -15,18 +15,21 @@
 #define BLUE_LEDSTP_PORT                  GPIOA
 #define BLUE_LEDSTP_PIN                   GPIO_PIN_3
 
-#define TIM4_PERIOD                       64
+#define TIM4_PERIOD                       255
 
 /* ----------------------------------------------------------------------------------------------------------*/
 
 #define RED_LEDSTP_ON                     GPIO_WriteHigh(RED_LEDSTP_PORT, RED_LEDSTP_PIN)
 #define RED_LEDSTP_OFF                    GPIO_WriteLow(RED_LEDSTP_PORT, RED_LEDSTP_PIN)
+#define READ_RED_LEDSTP                   GPIO_ReadInputPin(RED_LEDSTP_PORT, RED_LEDSTP_PIN)
 
-#define GREEN_LEDSTP_ON                     GPIO_WriteHigh(GREEN_LEDSTP_PORT, GREEN_LEDSTP_PIN)
-#define GREEN_LEDSTP_OFF                    GPIO_WriteLow(GREEN_LEDSTP_PORT, GREEN_LEDSTP_PIN)
+#define GREEN_LEDSTP_ON                   GPIO_WriteHigh(GREEN_LEDSTP_PORT, GREEN_LEDSTP_PIN)
+#define GREEN_LEDSTP_OFF                  GPIO_WriteLow(GREEN_LEDSTP_PORT, GREEN_LEDSTP_PIN)
+#define READ_GREEN_LEDSTP                 GPIO_ReadInputPin(GREEN_LEDSTP_PORT, GREEN_LEDSTP_PIN)
 
-#define BLUE_LEDSTP_ON                     GPIO_WriteHigh(BLUE_LEDSTP_PORT, BLUE_LEDSTP_PIN)
-#define BLUE_LEDSTP_OFF                    GPIO_WriteLow(BLUE_LEDSTP_PORT, BLUE_LEDSTP_PIN)
+#define BLUE_LEDSTP_ON                    GPIO_WriteHigh(BLUE_LEDSTP_PORT, BLUE_LEDSTP_PIN)
+#define BLUE_LEDSTP_OFF                   GPIO_WriteLow(BLUE_LEDSTP_PORT, BLUE_LEDSTP_PIN)
+#define READ_BLUE_LEDSTP                  GPIO_ReadInputPin(BLUE_LEDSTP_PORT, BLUE_LEDSTP_PIN)
 
 
 /*************************************************************************************************************/
@@ -38,14 +41,25 @@ void GPIO_init(void);
 void TMR4_init(void);
 
 /*************************************************************************************************************/
+/* --- VARIABLES --------------------------------------------------------------------------------------------*/
+
+uint8_t Red_PWM_Value = 0;
+uint8_t Green_PWM_Value = 0;
+uint8_t Blue_PWM_Value = TIM4_PERIOD / 2;
+
+/*************************************************************************************************************/
 /* --- INTERRUPT HANDLER ------------------------------------------------------------------------------------*/
 
 INTERRUPT_HANDLER(TIM4_UPD_OVF_IRQHandler, 23)
  {  
-    
-    GPIO_WriteReverse(BLUE_LEDSTP_PORT,BLUE_LEDSTP_PIN);
-
-    TIM4_ClearFlag(TIM4_FLAG_UPDATE);
+    if(READ_BLUE_LEDSTP){                                  // Is Output pin HIGH?
+      TIM4_SetCounter(Blue_PWM_Value);                      // Set PWM duty cycle low value
+      BLUE_LEDSTP_OFF;                                     // Turn off Output pin
+    }else{                                                 // Output pin is LOW
+      TIM4_SetCounter(TIM4_PERIOD - Blue_PWM_Value);        // Set PWM duty cycle high value
+      BLUE_LEDSTP_ON;                                      // Turn on Output pin
+    }
+    TIM4_ClearFlag(TIM4_FLAG_UPDATE);                      // Reset Overflow Flag
  }
 
 
@@ -59,7 +73,10 @@ int main( void )
 
   /* --- LOOP --- */
   while(1){
-    
+    for(int i = 0; i < TIM4_PERIOD; i += 5){
+      Blue_PWM_Value = i;
+      Delay_ms(50);
+    }
   }
 }
 
@@ -87,7 +104,7 @@ void OSC_init(void){
     CLK_HSICmd(ENABLE);							                                            // Internal Clock Enable - 16Mhz
     while(CLK_GetFlagStatus(CLK_FLAG_HSIRDY) == 0);
     CLK_ClockSwitchCmd(ENABLE);
-    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);                              // 16Mhz / 2 -> Fmaster = 8MHz
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV2);                              // 16Mhz / 2 -> Fmaster = 8MHz
     CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV1);                                    // 8MHz  -> Fcpu = 8MHz
     CLK_ClockSwitchConfig(CLK_SWITCHMODE_AUTO, CLK_SOURCE_HSI, DISABLE,CLK_CURRENTCLOCKSTATE_ENABLE); 
 
@@ -116,7 +133,7 @@ void TMR4_init(void){
 
     CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER4, ENABLE);
     /* Time base configuration */
-    TIM4_TimeBaseInit(TIM4_PRESCALER_1, TIM4_PERIOD);
+    TIM4_TimeBaseInit(TIM4_PRESCALER_16, TIM4_PERIOD);
     /* Clear TIM4 update flag */
     TIM4_ClearFlag(TIM4_FLAG_UPDATE);
     /* Enable update interrupt */
