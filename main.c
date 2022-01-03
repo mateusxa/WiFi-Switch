41932b9f3c6fe3e524dfe572b49aaf4cf96dd563
Mateus Xavier <mateus_xa@hotmail.com>
2022-01-03 16:51:35 -0300

    Timer 1 and 2 / Red abd Green PWM on aprox. 2 kHz

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 /* --- INCLUDES ---------------------------------------------------------------------------------------------*/
 #include "stm8s.h"
 #include "stm8s_delay.h"
+#include "math.h"
 
 /*************************************************************************************************************/
 /* --- DEFINE/MACRO -----------------------------------------------------------------------------------------*/
@@ -15,6 +16,8 @@
 #define BLUE_LEDSTP_PORT                  GPIOA
 #define BLUE_LEDSTP_PIN                   GPIO_PIN_3
 
+#define TIM1_PERIOD                       255
+#define TIM2_PERIOD                       255
 #define TIM4_PERIOD                       255
 
 /* ----------------------------------------------------------------------------------------------------------*/
@@ -38,17 +41,43 @@
 void MCU_init(void);
 void OSC_init(void);
 void GPIO_init(void);
+void TMR1_init(void);
+void TMR2_init(void);
 void TMR4_init(void);
 
 /*************************************************************************************************************/
 /* --- VARIABLES --------------------------------------------------------------------------------------------*/
 
-uint8_t Red_PWM_Value = 0;
-uint8_t Green_PWM_Value = 0;
-uint8_t Blue_PWM_Value = TIM4_PERIOD / 2;
+uint8_t Red_PWM_Value = 127;
+uint8_t Green_PWM_Value = 127;
+uint16_t Blue_PWM_Value = 127;
 
 /*************************************************************************************************************/
 /* --- INTERRUPT HANDLER ------------------------------------------------------------------------------------*/
+
+INTERRUPT_HANDLER(TIM1_UPD_OVF_TRG_BRK_IRQHandler, 11)
+ {  
+    if(READ_RED_LEDSTP){                                  // Is Output pin HIGH?
+      TIM1_SetCounter(Red_PWM_Value);                      // Set PWM duty cycle low value
+      RED_LEDSTP_OFF;                                     // Turn off Output pin
+    }else{                                                 // Output pin is LOW
+      TIM1_SetCounter(TIM1_PERIOD - Red_PWM_Value);        // Set PWM duty cycle high value
+      RED_LEDSTP_ON;                                      // Turn on Output pin
+    }
+    TIM1_ClearFlag(TIM1_FLAG_UPDATE);                      // Reset Overflow Flag
+ }
+
+INTERRUPT_HANDLER(TIM2_UPD_OVF_BRK_IRQHandler, 13)
+ {  
+    if(READ_GREEN_LEDSTP){                                  // Is Output pin HIGH?
+      TIM2_SetCounter(Green_PWM_Value);                      // Set PWM duty cycle low value
+      GREEN_LEDSTP_OFF;                                     // Turn off Output pin
+    }else{                                                 // Output pin is LOW
+      TIM2_SetCounter(TIM2_PERIOD - Green_PWM_Value);        // Set PWM duty cycle high value
+      GREEN_LEDSTP_ON;                                      // Turn on Output pin
+    }
+    TIM2_ClearFlag(TIM2_FLAG_UPDATE);                      // Reset Overflow Flag
+ }
 
 INTERRUPT_HANDLER(TIM4_UPD_OVF_IRQHandler, 23)
  {  
@@ -73,10 +102,11 @@ int main( void )
 
   /* --- LOOP --- */
   while(1){
-    for(int i = 0; i < TIM4_PERIOD; i += 5){
-      Blue_PWM_Value = i;
-      Delay_ms(50);
-    }
+    // for(int i = 0; i < TIM4_PERIOD; i += 5){
+    //   if(0.784*exp(0.023*i) > 245) Blue_PWM_Value = 245;
+    //   else Blue_PWM_Value = 0.784*exp(0.023*i);
+    //   Delay_ms(50);
+    // }
   }
 }
 
@@ -90,6 +120,8 @@ void MCU_init(void){
     OSC_init();
     GPIO_init();
     Delay_100us();
+    TMR1_init();
+    TMR2_init();
     TMR4_init();
     Delay_100us();
     enableInterrupts();
@@ -125,7 +157,45 @@ void GPIO_init(void){
 }
 
 /**
-  * @brief 769 kHz Timer | 1 Prescaler | TIM4_PERIOD
+  * @brief aprox 2 kHz Timer | 16 Prescaler | TIM2_PERIOD
+  */
+void TMR1_init(void){
+    
+    TIM1_DeInit();
+
+    CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER1, ENABLE);
+    /* Time base configuration */
+    TIM1_TimeBaseInit(16, TIM1_COUNTERMODE_UP, TIM1_PERIOD, 0);
+    /* Clear TIM1 update flag */
+    TIM1_ClearFlag(TIM1_FLAG_UPDATE);
+    /* Enable update interrupt */
+    TIM1_ITConfig(TIM1_IT_UPDATE, ENABLE);
+    /* Enable TIM1 */
+    TIM1_Cmd(ENABLE);    
+}
+
+/**
+  * @brief aprox 2 kHz Timer | 16 Prescaler | TIM2_PERIOD
+  */
+void TMR2_init(void){
+    
+    TIM2_DeInit();
+
+    CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER2, ENABLE);
+    /* Time base configuration */
+    TIM2_TimeBaseInit(TIM2_PRESCALER_16, TIM2_PERIOD);
+    /* Clear TIM2 update flag */
+    TIM2_ClearFlag(TIM2_FLAG_UPDATE);
+    /* Enable update interrupt */
+    TIM2_ITConfig(TIM2_IT_UPDATE, ENABLE);
+    /* Enable TIM2 */
+    TIM2_Cmd(ENABLE);    
+}
+
+/*************************************************************************************************************/
+
+/**
+  * @brief aprox 2 kHz Timer | 16 Prescaler | TIM4_PERIOD
   */
 void TMR4_init(void){
 
@@ -144,8 +214,8 @@ void TMR4_init(void){
 
     
 }
-/*************************************************************************************************************/
 
+/*************************************************************************************************************/
 #ifdef USE_FULL_ASSERT
 void assert_failed(uint8_t* file, uint32_t line)
 {
